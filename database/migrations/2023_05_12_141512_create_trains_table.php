f16aaa24e26eaa9b54e0b611519a7fa9aff29ea1
Francesco Rocco <francescorocco97@gmail.com>
2023-05-12 16:44:09 +0200

    first commit

---
+++ database/migrations/2023_05_12_141512_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 30);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_trains');
+    }
+};
